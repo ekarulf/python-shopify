dfd865976c35a7ee2ea4ff0eed49d6c0a1100043
Erik Karulf <erik@karulf.com>
2011-01-24 01:29:33 -0500

    Fixed Session initialization

--- src/shopify_api.py
+++ src/shopify_api.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # Copyright 2011 Erik Karulf. All Rights Reserved.
 
+import inspect
+import new
+
 from pyactiveresource.activeresource import ActiveResource
 from pyactiveresource.connection import Connection
 from shopify import util
@@ -8,33 +11,11 @@ from shopify import util
 def _remote_resources():
     remote = []
     resources = __import__('shopify.resources', fromlist=['__all__'])
-    for name in resources.__all__:
+    for name in dir(resources):
         resource = getattr(resources, name)
-        if issubclass(resource, ShopifyResource):
+        if inspect.isclass(resource) and issubclass(resource, ShopifyResource):
             remote.append(resource)
     return remote
-
-
-class Session(Connection):
-    def __new__(cls, *args, **kwargs):
-        for resource in _remote_resources():
-            wrapper = util.partial(cls._construct_resource, resource)
-            setattr(self, name, wrapper)
-        super(Session, cls).__new__(*args, **kwargs)
-
-    def __init__(self, domain, api_key, secret, token=None, protocol='https'):
-        user = api_key
-        if token:
-            password = util.md5(secret + token).hexdigest()
-        else:
-            password = secret
-        site = "%s://%s/admin/"
-        super(Session, self).__init__(site, user, password)
-
-    def _construct_resource(self, resource, *args, **kwargs):
-        instance = resource(*args, **kwargs)
-        instance.connection = instance.session = self
-        return instance
 
 
 class Countable(object):
@@ -69,4 +50,24 @@ class EventResource(object):
     def events(self):
         return self.session.Events.find(resource=self.plural,
                                         resource_id=self.id)
+
+
+# Session must remain at the bottom of the file to ease circular imports
+class Session(Connection):
+    def __init__(self, domain, api_key, secret, token=None, protocol='https'):
+        user = api_key
+        if token:
+            password = util.md5(secret + token).hexdigest()
+        else:
+            password = secret
+        site = "%s://%s/" % (protocol, domain)
+
+        super(Session, self).__init__(site, user, password)
+
+        for resource in _remote_resources():
+            name = resource.__name__
+            resource_class = new.classobj(name, (resource,), {})
+            resource_class._connection = resource_class.session = self
+            setattr(self, name, resource_class)
+
 
